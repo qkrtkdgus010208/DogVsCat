52f64e2a323f3d01b0538e5ba2431a5187b1a2e5
qkrtkdgus010208 <psh010208@naver.com>
2025-09-05 17:32:18 +0900

    Feature/level (#4)
    * 레벨업 표기
    
    * 레벨 반영

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,18 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
 
     [SerializeField] GameObject normalCat;
+    [SerializeField] GameObject fatCat;
     [SerializeField] GameObject retryButton;
+    [SerializeField] Text levelText;
+    [SerializeField] RectTransform levelFront;
+
+    private int level;
+    private int score;
 
     private void Awake()
     {
@@ -24,11 +31,34 @@ public class GameManager : MonoBehaviour
     private void MakeCat()
     {
         Instantiate(normalCat);
+        
+        if (level == 1)         // lv.1 20% 확률로 고양이를 더 생성
+        {
+            int p = Random.Range(0, 10);
+            if (p < 2) Instantiate(normalCat);
+        }
+        else if (level == 2)    // lv.2 50% 확률로 고양이를 더 생성
+        {
+            int p = Random.Range(0, 10);
+            if (p < 5) Instantiate(normalCat);
+        }
+        else if (level == 3)    // lv.3 뚱뚱한 고양이를 생성
+        {
+            Instantiate(fatCat);
+        }
     }
 
     public void GameOver()
     {
         Time.timeScale = 0f;
         retryButton.SetActive(true);
+    }
+
+    public void AddScore()
+    {
+        score++;
+        level = score / 5;
+        levelText.text = level.ToString();
+        levelFront.localScale = new Vector3((score % 5) / 5f, 1f, 1f);
     }
 }
