2cdbf044b313b09c75fa31a2c33ca78dc105527c
qkrtkdgus010208 <psh010208@naver.com>
2025-09-05 16:11:00 +0900

    강아지 밥 쏘기 구현

---
+++ Assets/Scripts/Food.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Food : MonoBehaviour
+{
+    private void Update()
+    {
+        transform.position += Vector3.up * 0.5f;
+    }
+
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
